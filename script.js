d54b79307bc8400f1016ec5413f3fc8a2d57a358
Fabiano Novais <fabiano.novais.99@gmail.com>
2022-09-10 16:59:12 -0300

    Laço de repetição For para progredir em um array

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 var listaFilmes = ["Yesterday", "A Chegada", "Escola de Rock"];
 
 //adicionando novos elementos
-listaFilmes.push("Harry Potter");
+listaFilmes.push("Harry Potter 2");
+listaFilmes.push("Harry Potter 7");
+listaFilmes.push("O Senhor dos Anéis");
 
-//escreve uma tag html no documento + seu conteúdo de forma simples
-// document.write("<p>" + listaFilmes[0] + "</p>");
-// document.write("<p>" + listaFilmes[1] + "</p>");
-// document.write("<p>" + listaFilmes[2] + "</p>");
-// document.write("<p>" + listaFilmes[3] + "</p>");
-
-console.log(listaFilmes.length);
-
-for (var indice = 0; indice <= 3; indice++) {
+// Laço de repetição For para progredir em um array
+for (var indice = 0; indice < listaFilmes.length; indice++) {
     document.write("<p>" + listaFilmes[indice] + "</p>");
+    document.write("<p>" + indice + "</p>");
 
 }
